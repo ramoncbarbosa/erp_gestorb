ff604de726644daa6cee2b8477ee805a87e78b18
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-01-15 00:03:29 -0300

    corrigido lib do types do express, criada código base do server.ts, criado express.d.ts para casos especificos, criado código do middleware de tratamento de erro e criado estrutura super simples de rotas raiz do projeto

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "express-async-errors";
+import { routes } from "./routes/routes";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+})
+
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`Server online na porta: ${port}`);
+})
